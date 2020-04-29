4120c53b4efc95ece3641c80858c0e3f433bc265
kbryant8 <kylebryant@Kyles-MacBook-Pro-3.local>
2020-04-29 14:11:20 -0400

    final edits by kyle
    main function written. Needs to string names inserted by DB person (notes will make clear where). Should be functional but had trouble testing

--- youtube.js
+++ youtube.js
@@ -1,7 +1,6 @@
 // reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-variable = "whatever song name is" + "artist name" + "music video"
 // getvideo searches for the youtube video desired, and embedVideo puts that on an html sit
-//we might have to display one song at a time using this code (user can select a song to appear)
+
 
 // function that calls the video. q is the song search, returns song and info
  function getVideo(variable) {
@@ -10,7 +9,7 @@ variable = "whatever song name is" + "artist name" + "music video"
         url: 'https://www.googleapis.com/youtube/v3/search',
         data: {
             key: 'AIzaSyCQBSBRJhWnl_R20nydi8IKyX96QXGp84k',
-            q: variable,
+            q: variable + "music video",
             part: 'snippet',
             maxResults: 1,
             type: 'video',
@@ -25,16 +24,19 @@ variable = "whatever song name is" + "artist name" + "music video"
       });
     }
 
-// not sure if this function will work
-function mult_songs(list_songs) {
-    var i;
-    for (i = 0; i < list_songs.length; i++) { 
-        getvideo(list_songs[i]);
-    }
-}
+
 // this should display video
 function embedVideo(data) {
     $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
     $('h3').text(data.items[0].snippet.title)
     $('.description').text(data.items[0].snippet.description)
 }
+
+// whoever did the DB should be able to swap that line and it should return an embedded music video. I had trouble testing this,
+// but the website I used for reference is reliable.
+function main() {
+    var variable = "whatever song name is" + "artist name"; // need database to do this, change each string to what it is in DB
+    var video = getVideo(variable);
+    embedVideo(video);
+}
+
